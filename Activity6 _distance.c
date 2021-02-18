f0d18a4b15d752ba669c38217cc10721d01aa0d7
adarshkk1 <78478543+adarshkk1@users.noreply.github.com>
2021-02-18 13:20:45 +0530

    Update Activity6 _distance.c

--- Activity6 _distance.c
+++ Activity6 _distance.c
@@ -1,13 +1,12 @@
 //WAP to find the distance between two point using 4 functions.
-
 #include<stdio.h>
 #include<math.h>
-float input()
+float input(char a[])
 {
-    float a; 
-    printf("Enter the value\n");
-    scanf("%f",&a);
-    return a;
+    float c; 
+    printf("Enter the value of %s \n",a);
+    scanf("%f",&c);
+    return c;
 }
 
 float find_distance(float x1, float x2 ,float y1,float y2)
@@ -17,19 +16,18 @@ float find_distance(float x1, float x2 ,float y1,float y2)
     return d;
 }
 
-void output(float a)
+void output(float d, float x1, float x2, float y1, float y2)
 {
-    printf("The distance between two points is %f \n",a);
+    printf("The distance between two points is %f \n",d);
 }
 int main()
 {
-    float x,y,x1,y1;
-    x=input();
-    y=input();
-    x1=input();
-    y1=input();
-    float z=find_distance(x,x1,y,y1);
-    output(z);
+    float x1,y1,x2,y2;
+    x1=input("x1");
+    x2=input("x1");
+    y1=input("y1");
+    y2=input("y2");
+    float d=find_distance(x1,x2,y1,y2);
+    output(d,x1,x2, y1,y2);
     return 0;
 }
-
