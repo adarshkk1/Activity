f41845427004ae4c508a0b4c4d453b5908858734
adarshkk1 <78478543+adarshkk1@users.noreply.github.com>
2021-04-07 23:32:46 +0530

    Update and rename act8_two_fractions.c to act8_two_fraction.c

---
+++ act8_two_fraction.c
@@ -0,0 +1,41 @@
+
+#include<stdio.h>
+struct fract
+{
+int  nume;
+int  deno;
+};
+typedef struct fract fraction;
+fraction input()
+{
+fraction a;
+printf("Enter the numerator\n");
+scanf("%d",&a.nume);
+printf("Enter the denominator\n");
+scanf("%d",&a.deno);
+return a;
+}
+fraction sum(fraction a,fraction b)
+{
+fraction res;
+if(a.deno==b.deno)
+{
+res.deno=a.deno;
+res.nume=a.nume+b.nume;
+}
+else 
+{
+res.deno=a.deno*b.deno;
+res.nume=(a.nume*b.deno)+(b.nume*a.deno);
+}
+return res;
+}
+int main()
+{
+fraction a,b,c;
+a=input();
+b=input();
+c=sum(a,b);
+printf("The Total sum is %d/%d",c.nume,c.deno);
+return 0;
+}
