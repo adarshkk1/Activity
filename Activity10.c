e75aaa8d48344211ed3a38a78680cb468b20bfe2
adarshkk1 <78478543+adarshkk1@users.noreply.github.com>
2021-04-08 17:31:55 +0530

    Update Activity10.c

--- Activity10.c
+++ Activity10.c
@@ -1,52 +1,71 @@
 //activity 10
-#include<stdio.h>
-struct fract
+#include <stdio.h>
+struct fraction
 {
-    int  nume;
-    int  deno;
+    int nume;
+    int deno;
 };
-typedef struct fract fraction;
-fraction input()
+typedef struct fraction fract;
+
+void input(fract array[],int n)
 {
-    fraction a;
-    printf("enter the numerator\n");
-    scanf("%d",&a.nume);
-    printf("enter the denominator\n");
-    scanf("%d",&a.deno);
-    return a;
+    int i;
+    for(i=0;i<n;i++)
+    {
+        printf("enter numerator: \n ");
+        scanf("%d",&array[i].nume);
+        printf("enter denominator: \n");
+        scanf("%d",&array[i].deno);
+    }
 }
-fraction sum(fraction a,fraction b)
+
+int gcd(int a, int b)
 {
-    fraction res;
-    if(a.deno==b.deno)
+    int i,gcd=1;
+    for(i=2; i <= a && i <= b; ++i)
     {
-        res.deno=a.deno;
-        res.nume=a.nume+b.nume;
+        if(a%i==0 && b%i==0)
+        gcd = i;
     }
-    else
-    {
-        res.deno=a.deno*b.deno;
-        res.nume=(a.nume*b.deno)+(b.nume*a.deno);
-    }
-    return res;
+    return gcd;
 }
+
+fract fraction(fract arr[],int n)
+{
+    fract sum,f;
+    int Gcd;
+    f.nume=arr[0].nume;
+    f.deno=arr[0].deno;
+    for(int i=1;i<n;i++)
+    {
+        sum.deno=f.deno*arr[i].deno;
+        sum.nume=(f.nume*arr[i].deno)+(arr[i].nume*f.deno);
+        Gcd=gcd(sum.nume,sum.deno);
+        sum.deno=(sum.deno)/Gcd;
+        sum.nume=(sum.nume)/Gcd;
+        f=sum;
+    }
+    return sum;
+}
+
+void output(int n,fract array[],fract sum)
+{
+    for(int i=0;i<n-1;i++)
+    {
+        printf("%d/%d+ ",array[i].nume,array[i].deno);
+    }
+    printf("%d/%d = %d/%d",array[n-1].nume,array[n-1].deno,sum.nume,sum.deno);
+}
+
 int main()
 {
-    int n;
-    printf("enter the number of fractions to be add\n");
+    int n,i;
+    printf("Enter the number of fractions:\n");
     scanf("%d",&n);
-    fraction c,a[n];
-    c.nume=0;
-    c.deno=1;
-    for(int i=0;i<n;i++)
-    {
-        printf("for fraction %d ",(i+1));
-        a[i]=input();
-    }
-    for(int i=0;i<n;i++)
-    {
-        c=sum(c,a[i]);
-    }
-    printf("The Total sum is %d/%d",c.nume,c.deno);
+    fract array[n],sum;
+    input(array,n);
+    sum=fraction(array,n);
+    output(n,array,sum);
     return 0;
-}
+} 
+
